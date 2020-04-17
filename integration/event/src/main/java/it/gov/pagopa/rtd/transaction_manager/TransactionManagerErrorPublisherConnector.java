4a35cd9aa9f9db944e73e5126fd51a72511d3ec3
Alessio Cialini <acialini@everis.com>
2020-04-17 19:34:10 +0200

    [rtd-transaction-manager/develop] Introduced Test for error publisher, update default Rest connector configuration

--- integration/event/src/main/java/it/gov/pagopa/rtd/transaction_manager/TransactionManagerErrorPublisherConnector.java
+++ integration/event/src/main/java/it/gov/pagopa/rtd/transaction_manager/TransactionManagerErrorPublisherConnector.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class TransactionManagerErrorPublisherConnector
         extends BaseEventConnector<byte[], Boolean, byte[], Void> {
+
 }
---
+++ integration/event/src/test/java/it/gov/pagopa/rtd/transaction_manager/TransactionManagerErrorPublisherConnectorTest.java
@@ -0,0 +1,41 @@
+package it.gov.pagopa.rtd.transaction_manager;
+
+import eu.sia.meda.event.BaseEventConnectorTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+
+import java.nio.charset.StandardCharsets;
+
+@Import({TransactionManagerErrorPublisherConnector.class})
+@TestPropertySource(
+        locations = "classpath:config/testTransactionManagerErrorPublisher.properties",
+        properties = {
+                "connectors.eventConfigurations.items.TransactionManagerErrorPublisherConnector.bootstrapServers=${spring.embedded.kafka.brokers}"
+        })
+public class TransactionManagerErrorPublisherConnectorTest extends
+        BaseEventConnectorTest<byte[], Boolean, byte[], Void, TransactionManagerErrorPublisherConnector> {
+
+    @Value("${connectors.eventConfigurations.items.TransactionManagerErrorPublisherConnector.topic}")
+    private String topic;
+
+    @Autowired
+    private TransactionManagerErrorPublisherConnector transactionManagerErrorPublisherConnector;
+
+    @Override
+    protected TransactionManagerErrorPublisherConnector getEventConnector() {
+        return transactionManagerErrorPublisherConnector;
+    }
+
+    @Override
+    protected byte[] getRequestObject() {
+        return "error".getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    protected String getTopic() {
+        return topic;
+    }
+
+}
